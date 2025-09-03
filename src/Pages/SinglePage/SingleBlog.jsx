14b99a74fae483846119685291d930da3ea3c456
tauhidul <tauhidul.islam.tain18@gmail.com>
2025-09-03 20:30:13 +0600

    Singel Blog Page Design

--- src/Pages/SinglePage/SingleBlog.jsx
+++ src/Pages/SinglePage/SingleBlog.jsx
@@ -27,11 +27,35 @@ const SingleBlog = () => {
   if (error) return <p style={{ color: "red" }}>{error}</p>;
 
   return (
-    <div>
-      <h1>{blog.title}</h1>
-      <p>
-        By {blog.user.name} in {blog.category.name}
-      </p>
+    <div className="container col-12 mt-3 mb-3">
+      <img
+        className="col-12"
+        style={{ maxHeight: "450px", objectFit: "cover" }}
+        src={blog.image}
+        alt=""
+      />
+      <h1 className="mt-2">{blog.title}</h1>
+      <div className="col-12 d-flex justify-content-between">
+        <div className="d-flex gap-1 ">
+          <p>By </p>
+          <p style={{ fontWeight: "bold" }}> {blog.user.name}</p>
+        </div>
+        <div className="d-flex gap-1">
+          In the category
+          <p style={{ fontWeight: "bold" }}> {blog.category.category_name}</p>
+        </div>
+      </div>
+      <div className="d-flex gap-1 mb-3" style={{ lineHeight: "0px" }}>
+        Published in:{" "}
+        <p style={{ fontWeight: "bold" }}>
+          {new Date(blog.created_at).toLocaleDateString("en-GB", {
+            day: "numeric",
+            month: "long",
+            year: "numeric",
+          })}
+        </p>
+      </div>
+
       <div>{blog.content}</div>
     </div>
   );
